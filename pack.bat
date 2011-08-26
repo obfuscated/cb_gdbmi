82020552052528a96f908e40db95ac8629a0e5c2
Teodor Petrov <fuscated@gmail.com>
2011-08-26 23:27:04 +0000

    Improved the project and script files

--- pack.bat
+++ pack.bat
@@ -1,5 +1,5 @@
 SET CB_SDK=%1\devel\share\codeblocks
 
 if exist %CB_SDK%\debugger_gdbmi.zip del /f /q %CB_SDK%\debugger_gdbmi.zip
-copy debugger_gdbmi.dll %CB_SDK%\debugger_gdbmi.dll
+copy debugger_gdbmi.dll %CB_SDK%\plugins\debugger_gdbmi.dll
 zip -j9 %CB_SDK%\debugger_gdbmi.zip resource\manifest.xml resource\*.xrc
